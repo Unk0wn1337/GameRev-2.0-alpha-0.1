cf4280dc448bffc6f7226651ebb0437a29f6c4f1
malackapite <veress.marcell@protonmail.com>
2022-11-22 19:41:47 +0100

    animation + audio fix

--- js.js
+++ js.js
@@ -1,9 +1,15 @@
-for (let ix = 0; ix < 14; ix++) {
+for (let ix = 0; ix < window.innerWidth/128-1; ix++) {
     document.getElementsByTagName('nav')[0].innerHTML+=`<img src="kepek/lolface.png" alt="LOLCSI" class="lolCsika">`
     
 }
 
 function beUgras()
 {
-    document.getElementById('egy').classList.add="beugras"
+    document.getElementById('egy').classList.add("beugras")
+    document.getElementById('egy').classList.remove("kiugras")
+}
+function kiUgras()
+{
+    document.getElementById('egy').classList.add("kiugras")
+    document.getElementById('egy').classList.remove("beugras")
 }
