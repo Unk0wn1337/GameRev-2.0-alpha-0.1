0ee8e1f2d4612b6c736b80128f432ce3c395f14d
malackapite <veress.marcell@protonmail.com>
2022-11-22 12:12:06 +0100

    minor issue fixed

--- js.js
+++ js.js
@@ -1,4 +1,10 @@
+for (let ix = 0; ix < window.innerWidth/128-1; ix++) {
+    document.getElementsByTagName('nav')[0].innerHTML+=`<img src="kepek/lolface.png" alt="LOLCSI" class="lolCsika">`;
+}
+
+
 function beUgras()
 {
-    document.getElementById('egy').classList.add="beugras"
+    document.getElementById('egy').classList.add("beugras")
+    console.log("sdljksdégl")
 }
